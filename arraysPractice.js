3cc0dc430f45237580404ea29c3827a0fc4159f6
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-09 17:02:57 -0700

    update

--- arraysPractice.js
+++ arraysPractice.js
@@ -5,36 +5,59 @@ var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item the given array.
 
   //Code Here
+var first = function() {
+  return arr[0];
+};
+
+first();
+
 
 
 //Next problem
-
-
 
 var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item the given array.
 
 
   //Code Here
+var last = function() {
+  return arr[arr.length - 1];
+};
+
+last();
+
 
 
 //Next Problem
-
 
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
   //Code Here
+var looper = function() {
+  for (var i = 0; i < family.length; i++) {
+    alert(family[i]);
+  };
+};
+
+looper();
+
 
 
 //Next problem
-
-
 
 var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
   //Code Here
+var reversedLooper = function() {
+  for (var i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i]);
+  };
+};
+
+reversedLooper();
+
 
 
 //Next Problem
@@ -44,10 +67,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
   //Code Here
+var evenFinder = function() {
+  for (var i = 0; i < nums.length; i++) {
+
+  }  
+}
+
 
 
 //Next problem
-
 
 var nums = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 var evens = [];
